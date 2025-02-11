7780ddf90308f8fade5fcdc59a4f9bda9668e749
Miguel Sierra <miguelsierrac@gmail.com>
2025-02-11 13:20:43 -0500

    Integrate Firebase library and disable Server-Side Rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,25 @@
 	import { goto } from '$app/navigation';
 	import { base } from '$app/paths';
 	import { browser } from '$app/environment';
-	import { setContext } from 'svelte';
+	import { onMount, setContext } from 'svelte';
 	import { athlete, lastSync, token } from '../stores.js';
+	import { initializeApp } from 'firebase/app';
+	import { getMessaging, getToken, onMessage } from 'firebase/messaging';
+
+	const firebaseConfig = {
+		apiKey: 'AIzaSyD2JWxdRU6AhI5WMBHgvLMb6v8x9tLzqw0',
+		authDomain: 'coral-swimmer.firebaseapp.com',
+		projectId: 'coral-swimmer',
+		storageBucket: 'coral-swimmer.appspot.com',
+		messagingSenderId: '528677262049',
+		appId: '1:528677262049:web:89c2b229471e4cef505da0',
+		measurementId: 'G-Q61LGSCMEX'
+	};
+
+	console.log('Initialize Firebase...');
+	// Initialize Firebase
+	const app = initializeApp(firebaseConfig);
+	const messaging = getMessaging(app);
 
 	const provider = Provider;
 
@@ -23,97 +40,60 @@
 	setContext('athlete', athlete);
 	setContext('lastSync', lastSync);
 	setContext('token', token);
-</script>
 
-<svelte:head>
-	<script type="module">
-		// Import the functions you need from the SDKs you need
-		import { initializeApp } from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js';
-		import { getAnalytics } from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-analytics.js';
-		import {
-			getMessaging,
-			getToken,
-			onMessage
-		} from 'https://www.gstatic.com/firebasejs/11.3.0/firebase-messaging.js';
-
-		// TODO: Add SDKs for Firebase products that you want to use
-		// https://firebase.google.com/docs/web/setup#available-libraries
-
-		// Your web app's Firebase configuration
-		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-		const firebaseConfig = {
-			apiKey: 'AIzaSyD2JWxdRU6AhI5WMBHgvLMb6v8x9tLzqw0',
-			authDomain: 'coral-swimmer.firebaseapp.com',
-			projectId: 'coral-swimmer',
-			storageBucket: 'coral-swimmer.appspot.com',
-			messagingSenderId: '528677262049',
-			appId: '1:528677262049:web:89c2b229471e4cef505da0',
-			measurementId: 'G-Q61LGSCMEX'
-		};
-
-		// Initialize Firebase
-		const app = initializeApp(firebaseConfig);
-		const analytics = getAnalytics(app);
-		const messaging = getMessaging(app);
+	async function requestNotificationPermission() {
 		console.log('Requesting permission...');
-		Notification.requestPermission().then((permission) => {
-			if (permission === 'granted') {
-				console.log('Notification permission granted.');
+		let permission = await Notification.requestPermission();
 
-				if ('serviceWorker' in navigator) {
-					navigator.serviceWorker
-						.register('../firebase-messaging-sw.js')
-						.then(function (registration) {
-							console.log('Registration successful, scope is:', registration.scope);
+		return permission === 'granted';
+	}
 
-							getToken(messaging, {
-								vapidKey:
-									'BIxDq1HszvHFhbBARVJcPokYfOFQo74ZAzq2LZCBVn2K740j0TnvCJMBAgHweAGIG5bPlJTfHcE9cdr_du4Gqc4'
-							})
-								.then((currentToken) => {
-									if (currentToken) {
-										// Send the token to your server and update the UI if necessary
-										// ...
-										console.log('Token available: ', currentToken);
-										localStorage.setItem('TOKEN', '"' + currentToken + '"');
-									} else {
-										// Show permission request UI
-										console.log(
-											'No registration token available. Request permission to generate one.'
-										);
-										// ...
-									}
-								})
-								.catch((err) => {
-									console.log('An error occurred while retrieving token. ', err);
-									// ...
-								});
+	onMount(async () => {
+		let permissionGranted = await requestNotificationPermission();
+		if (permissionGranted) {
+			console.log('Notification permission granted.');
 
-							onMessage(messaging, (payload) => {
-								console.log('Message received. ', payload);
+			console.log('Messaging get token...');
+			getToken(messaging, {
+				vapidKey:
+					'BIxDq1HszvHFhbBARVJcPokYfOFQo74ZAzq2LZCBVn2K740j0TnvCJMBAgHweAGIG5bPlJTfHcE9cdr_du4Gqc4'
+			})
+				.then((currentToken) => {
+					if (currentToken) {
+						// Send the token to your server and update the UI if necessary
+						// ...
+						console.log('Token available: ', currentToken);
+						$token = currentToken;
+					} else {
+						// Show permission request UI
+						console.log('No registration token available. Request permission to generate one.');
+						// ...
+					}
+				})
+				.catch((err) => {
+					console.log('An error occurred while retrieving token. ', err);
+					// ...
+				});
 
-								const notificationTitle = payload.notification.title;
-								const notificationOptions = {
-									body: payload.notification.body,
-									icon: '/logo_512.png'
-								};
+			onMessage(messaging, (payload) => {
+				console.log('Message received. ', payload);
 
-								var notification = new Notification(notificationTitle, notificationOptions);
-								notification.onclick = () => {
-									notification.close();
-									window.parent.focus();
-								};
-							});
-						})
-						.catch(function (err) {
-							console.log('Service worker registration failed, error:', err);
-						});
-				}
-			} else {
-				console.log('Unable to get permission to notify.');
-			}
-		});
-	</script>
-</svelte:head>
+				const notificationTitle = payload.notification.title;
+				const notificationOptions = {
+					body: payload.notification.body,
+					icon: '/logo_512.png'
+				};
+
+				var notification = new Notification(notificationTitle, notificationOptions);
+				notification.onclick = () => {
+					notification.close();
+					window.parent.focus();
+				};
+			});
+		} else {
+			console.log('Unable to get permission to notify.');
+		}
+	});
+</script>
 
 <slot />
